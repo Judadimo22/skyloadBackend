4d0ba99a52ad93519d07ae2bd69f11632b0e2f2f
Juan David Díaz <diazmorenodavid16@gmail.com>
2026-03-10 18:39:16 -0500

    Archivo configuracion firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json"); 
+const serviceAccount = require("./firebase-config.json"); 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
